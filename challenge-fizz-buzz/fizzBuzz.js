e91176bd596f5e6f38bff20d3e7d09898aac7e1f
sujala <117114233+sujala29@users.noreply.github.com>
2022-11-10 07:55:56 -0600

    fizz buzz file

---
+++ challenge-fizz-buzz/fizzBuzz.js
@@ -0,0 +1,17 @@
+// Write your solution below this line:
+
+for (i=0;i<=50;i++){
+    if(i % 3 === 0 && i % 5 ===0){
+        console.log("FizzBuzz");
+    }
+    else if(i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if(i % 5 === 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+
+    }
